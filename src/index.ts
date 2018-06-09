cfb3dc7a8fc6669b1a535434a7329bd480b043ea
Wayne Bloss <waynebloss@gmail.com>
2018-06-09 13:49:26 -0400

    Use `exports = ` to create a Node.js compatible CommonJS module.

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,12 @@
 /// <reference path="polyfill.d.ts" />
+import {
+  EventHandle,
+  EventHandleConfiguration,
+  EventHandleFunction,
+  EventHandler,
+  EventHandlerOptions,
+  EventHandlerRemover,
+} from './types';
 
 // NOTE: The default export is known as EventHandleExports internally, so as
 // not to clash with the type, also named EventHandle.
@@ -112,40 +120,7 @@ const EventHandleExports = {
     return fn && typeof fn === 'function' && typeof fn.handle === 'function';
   },
 }
-export default EventHandleExports;
-
-/** Core function that triggers the event. */
-export type EventHandleFunction = (...args: any[]) => void;
-
-/** Function with extended properties that triggers the event. */
-export interface EventHandle extends EventHandleFunction {
-  handle: (handler: EventHandler, options?:EventHandlerOptions) => EventHandlerRemover;
-  handlerCount: () => number;
-  id?:string;
-  removeAllHandlers: () => void;
-}
-
-export interface EventHandleConfiguration {
-  /** Event identifier (typically a name). */
-  id?: string;
-  /** Function to be called before event handlers. */
-  before?(...args: any[]): void;
-  /** Function to be called after event handlers. */
-  after?(...args: any[]): void;
-}
-
-/** Function that will handle an event. */
-export type EventHandler = (...args: any[]) => void;
-
-export interface EventHandlerOptions {
-  /** True if the handler should be called only once.  */
-  once?: boolean;
-  /** True if the handler should be inserted first. */
-  prepend?: boolean;
-}
-
-/** Removes an `EventHandler` from its `EventHandle` so it won't be called. */
-export type EventHandlerRemover = () => boolean;
+export = EventHandleExports;
 
 function tryRemoveHandler(handlers: EventHandler[] | undefined, handler: EventHandler) {
   if (!handlers) return false;
